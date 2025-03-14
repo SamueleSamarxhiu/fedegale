5f16053d08c074cec541085752181ce34cbe788c
Samuele <73663949+SamueleSamarxhiu@users.noreply.github.com>
2025-03-14 11:33:01 +0100

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-const audio = document.getElementById('click');
-
-
-document.querySelectorAll('.trigger').forEach(element => {
-  element.addEventListener('mouseenter', () => {
-    audio.currentTime = 0;
-    audio.play();
-  });
-});
