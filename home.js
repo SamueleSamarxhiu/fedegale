163a6f2f82413e23d4edba4d348a86b2a679f6e5
Samuele <73663949+SamueleSamarxhiu@users.noreply.github.com>
2025-03-15 22:53:16 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,20 @@
+const loader = document.querySelector('.loader');
+const audio = document.getElementById('click');
+
+function playSound() {
+  audio.currentTime = 0;
+  audio.play();
+}
+
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    loader.style.animation = 'loadIn 1.5s linear forwards';
+  }, 5000);
+});
+
+
+document.querySelectorAll('.trigger').forEach(element => {
+  element.addEventListener('mouseenter', playSound);
+  element.addEventListener('touchstart', playSound, {passive: true});
+});
